d756e2bee9e82e69369eade0afaf9edbe47267b5
Sameer Taneja <sameertaneja@Sameers-MacBook-Air.local>
2025-05-18 01:54:23 +0530

    sam build 3

--- src/app/metadata.tsx
+++ src/app/metadata.tsx
@@ -1,7 +1,7 @@
 // src/app/metadata.ts
 export const metadata = {
     title: 'Super Teacher',
-    description: 'This is Super Teacher.',
+            description: "India's First AI grading co-pilot for teachers.",
     icons: {
       icon: '/favicon.png', // Make sure this file is placed in the /public folder
     },
